63855708991afd0675134b1cd44900279131ed85
José Miguel Morillo <josemiguel.morillo@criterian.es>
2018-01-17 07:27:10 +0100

    Track assets load files

---
+++ assets/FontAsset.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josemiguelmorillocallejon
+ * Date: 17/01/18
+ * Time: 05:54
+ */
+
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+class FontAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'fonts.googleapis.com/css?family=Dosis|Roboto',
+    ];
+    public $cssOptions = [
+        'type' => 'text/css',
+    ];
+}
