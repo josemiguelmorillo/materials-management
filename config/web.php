ef75558174471237604d341bbcd6b554eab08601
José Miguel Morillo <josemiguel.morillo@criterian.es>
2018-01-17 07:23:57 +0100

    Change app name

--- config/web.php
+++ config/web.php
@@ -5,7 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
-    'name' => 'Materials',
+    'name' => 'Ge-Ma',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
